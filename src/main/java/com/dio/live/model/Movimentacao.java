db86c80f7987148423b61e143cbf48557d7a74da
renatoabreu14 <renato.abreu.info@gmail.com>
2021-09-06 20:51:33 -0300

    Parte 8

--- src/main/java/com/dio/live/model/Movimentacao.java
+++ src/main/java/com/dio/live/model/Movimentacao.java
@@ -2,8 +2,7 @@ package com.dio.live.model;
 
 import lombok.*;
 
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,6 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
+@Entity
 public class Movimentacao {
 
     @AllArgsConstructor
@@ -24,13 +24,15 @@ public class Movimentacao {
         private long idMovimento;
         private long idUsuario;
     }
-
+    @Id
     @EmbeddedId
     private MovimentacaoId id;
     private LocalDateTime dataEntrada;
     private LocalDateTime dataSaida;
     private BigDecimal periodo;
+    @ManyToOne
     private Ocorrencia ocorrencia;
+    @ManyToOne
     private Calendario calendario;
 
 }
