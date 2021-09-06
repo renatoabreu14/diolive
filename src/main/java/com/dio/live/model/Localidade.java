6d1992354bf14e131028751e78eb2dad1e5d6750
renatoabreu14 <renato.abreu.info@gmail.com>
2021-09-06 15:35:38 -0300

    Até a parte 4

---
+++ src/main/java/com/dio/live/model/Localidade.java
@@ -0,0 +1,18 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.OneToMany;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Localidade {
+    private long id;
+    @OneToMany
+    private NivelAcesso nivelAcesso;
+    private String descricao;
+}
