2668dcafbe1e7b04bec7ddbb20a8fe386734b9d1
renatoabreu14 <renato.abreu.info@gmail.com>
2021-09-06 16:53:26 -0300

    Parte 5

--- src/main/java/com/dio/live/model/TipoData.java
+++ src/main/java/com/dio/live/model/TipoData.java
@@ -2,17 +2,13 @@ package com.dio.live.model;
 
 import lombok.*;
 
-import javax.persistence.ManyToOne;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
-public class Localidade {
+public class TipoData {
     private long id;
-    @ManyToOne
-    private NivelAcesso nivelAcesso;
     private String descricao;
 }
--- src/main/java/com/dio/live/model/Usuario.java
+++ src/main/java/com/dio/live/model/Usuario.java
@@ -2,6 +2,7 @@ package com.dio.live.model;
 
 import lombok.*;
 
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -13,10 +14,14 @@ import java.time.LocalDateTime;
 @Builder
 public class Usuario {
     private long id;
+    @ManyToOne
     private CategoriaUsuario categoriaUsuario;
     private String nome;
+    @ManyToOne
     private Empresa empresa;
+    @ManyToOne
     private NivelAcesso nivelAcesso;
+    @ManyToOne
     private JornadaTrabalho jornadaTrabalho;
     private BigDecimal tolerancia;
     private LocalDateTime inicioJornada;
